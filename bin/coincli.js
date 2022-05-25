d187093febfbc2a5f5a06c39e058d38646aa3477
chriscaracach <christiancaracach@gmail.com>
2022-05-25 09:45:18 -0300

    final

--- bin/coincli.js
+++ bin/coincli.js
@@ -1,7 +1,17 @@
 #!/usr/bin/env node
 const program = require("commander");
+const check = require("../commands/check");
 
 program
   .version("1.0")
-  .command("check", "Check Coin Price Info")
-  .parse(process.argv);
+  .command("check")
+  .option(
+    "--coin <type>",
+    "Check coin price (Bitcoin, ethereum, dogecoin)",
+    "bitcoin"
+  )
+  .action((cmd) => {
+    check.price(cmd);
+  });
+
+program.parse(process.argv);
--- commands/check.js
+++ commands/check.js
@@ -1,6 +1,17 @@
+const CoinAPI = require("../lib/coinAPI");
+const colors = require("colors");
+
 const check = {
-  price(cmd) {
-    console.log(cmd);
+  async price(cmd) {
+    try {
+      const api = new CoinAPI();
+      const priceData = await api.getCoinData(cmd.coin);
+      const formattedResult =
+        "The current price of " + priceData.name + " is: $" + priceData.price;
+      console.log(formattedResult.green);
+    } catch (error) {
+      console.log(error.message.red);
+    }
   },
 };
 
